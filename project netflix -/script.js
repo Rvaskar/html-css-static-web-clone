d193e8c5f6346e2f5fb69c9cb2d28d106d7bf1d3
RutikVaskar <rutikkvaskar9@gmail.com>
2024-06-18 12:08:46 +0530

    media quesry

---
+++ project netflix -/script.js
@@ -0,0 +1,70 @@
+// function toggleClass() {
+//   let element = document.querySelector('.cont5-text');
+//   if (element.classList.contains('cont5-text')) {
+//     element.classList.remove('cont5-text');
+//     element.classList.add('show-text');
+//   } else {
+//     element.classList.remove('show-text');
+//     element.classList.add('cont5-text');
+//   }
+// }
+function toggleClass() {
+  let element = document.getElementById('li1');
+  element.classList.toggle('cont5-text');
+  element.classList.toggle('show-text');
+
+  let element11 = document.getElementById('img1');
+  element11.classList.toggle('rotate-img1');
+  element11.classList.toggle('rotate-img11');
+}
+
+
+function toggleClass2() {
+  let element2 = document.getElementById('li2');
+  element2.classList.toggle('cont5-text2');
+  element2.classList.toggle('show-text2');
+
+  let element71 = document.getElementById('img7');
+  element71.classList.toggle('rotate-img7');
+  element71.classList.toggle('rotate-img71');
+}
+
+
+function toggleClass3() {
+  let element2 = document.getElementById('li3');
+  element2.classList.toggle('cont5-text3');
+  element2.classList.toggle('show-text3');
+
+  let element31 = document.getElementById('img3');
+  element31.classList.toggle('rotate-img3');
+  element31.classList.toggle('rotate-img31');
+}
+
+function toggleClass4() {
+  let element2 = document.getElementById('li4');
+  element2.classList.toggle('cont5-text4');
+  element2.classList.toggle('show-text4');
+
+  let element41 = document.getElementById('img4');
+  element41.classList.toggle('rotate-img4');
+  element41.classList.toggle('rotate-img41');
+}
+function toggleClass5() {
+  let element2 = document.getElementById('li5');
+  element2.classList.toggle('cont5-text5');
+  element2.classList.toggle('show-text5');
+
+  let element51 = document.getElementById('img5');
+  element51.classList.toggle('rotate-img5');
+  element51.classList.toggle('rotate-img51');
+}
+
+function toggleClass6() {
+  let element2 = document.getElementById('li6');
+  element2.classList.toggle('cont5-text6');
+  element2.classList.toggle('show-text6');
+
+  let element61 = document.getElementById('img6');
+  element61.classList.toggle('rotate-img6');
+  element61.classList.toggle('rotate-img61');
+}
